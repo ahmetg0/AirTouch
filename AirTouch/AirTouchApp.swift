78c665e236f621f96417d7e2498f3c48067f3217
ahmetg0 <ahmetcemalzero@gmail.com>
2026-03-03 14:07:48 -0800

    Initial Commit

---
+++ AirTouch/AirTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirTouchApp.swift
+//  AirTouch
+//
+//  Created by Ahmet Gundogdu on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AirTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
